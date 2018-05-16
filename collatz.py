ba49d7a9bae5b25297ae9c055b56a1672a5bb88a
Caleb Sangwin <c.sangwin@hotmail.com>
2018-05-16 14:36:58 -0400

    Changed step count into a function.

--- collatz.py
+++ collatz.py
@@ -19,20 +19,26 @@ def steps(n):
     steps = len(collatz(n))
     return steps
 
-all_paths = []
-step_touch_count = {}
-for i in range(2, 101):
-    path = collatz(i)
-    all_paths.append(path)
+def step_in_path_count(n):
+    '''Count how many paths each step was a part of. Counts all all_paths
+    from 2 to n.'''
+    all_paths = []
+    step_touch_count = {}
+    for i in range(2, n + 1):
+        path = collatz(i)
+        all_paths.append(path)
 
-for path in all_paths:
-    for step in path:
-        if step in step_touch_count:
-            step_touch_count[step] += 1
-        else:
-            step_touch_count[step] = 1
+    for path in all_paths:
+        for step in path:
+            if step in step_touch_count:
+                step_touch_count[step] += 1
+            else:
+                step_touch_count[step] = 1
+    return step_touch_count
 
-s = pd.Series(step_touch_count, name = 'Step Touches')
+step_touches = step_in_path_count(10)
+
+s = pd.Series(step_touches, name = 'Step Touches')
 s.index.name = 'Step'
 s.reset_index()
 s.plot(xlim = (0,100))
